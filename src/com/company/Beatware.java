23aa329d447afe372935c57f6e14c202bdc1aa5a
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-11 00:08:14 +0900

    Initial commit

---
+++ src/com/company/Beatware.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Beatware extends JFrame {
+
+    private Image screenImage;
+    private Graphics screenGraphic;
+    private Image introBackground = new ImageIcon(Main.class.getResource("../../images/introBackground.jpg")).getImage();
+    private JLabel menuBar= new JLabel(new ImageIcon(Main.class.getResource("../../images/menu.png")));
+
+    public Beatware(){
+        setUndecorated(true);
+        setTitle("Beatware");
+        setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setBackground(new Color(0,0,0,0));
+        setLayout(null);
+
+        menuBar.setBounds(0,0,1280,30);
+        add(menuBar);
+
+        Music introMusic = new Music("introMusic.mp3", true);
+        introMusic.start();
+    }
+    public void paint(Graphics g) {
+        screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        screenGraphic = screenImage.getGraphics();
+        screenDraw(screenGraphic);
+        g.drawImage(screenImage, 0, 0, null);
+    }
+    public void screenDraw(Graphics g){
+        g.drawImage(introBackground, 0, 0, null);
+        paintComponents(g);
+
+        this.repaint();
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.io.IOException;
+
+public class Main {
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+
+    public static void main(String[] args) throws IOException {
+        new Beatware();
+    }
+
+}
