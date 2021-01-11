ccbc38b9abddab36359c00afee5fee94a38d6e69
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-12 01:26:34 +0900

    Add exitbutton

--- src/com/company/Beatware.java
+++ src/com/company/Beatware.java
@@ -9,6 +9,8 @@ public class Beatware extends JFrame {
     private Graphics screenGraphic;
     private Image introBackground = new ImageIcon(Main.class.getResource("../../images/introBackground.jpg")).getImage();
     private JLabel menuBar= new JLabel(new ImageIcon(Main.class.getResource("../../images/menu.png")));
+    private JButton exitButton = new JButton(new ImageIcon(Main.class.getResource("../../images/exitButtonBasic.png")));
+    private int mouseX, mouseY;
 
     public Beatware(){
         setUndecorated(true);
@@ -23,6 +25,12 @@ public class Beatware extends JFrame {
 
         menuBar.setBounds(0,0,1280,30);
         add(menuBar);
+
+        exitButton.setBounds(50,50,30,30);
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setFocusPainted(false);
+        add(exitButton);
 
         Music introMusic = new Music("introMusic.mp3", true);
         introMusic.start();
